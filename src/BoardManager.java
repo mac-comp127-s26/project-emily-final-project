3c8d82793f43dd378c909b253ce2794d81444c28
emilyk08 <emk.emily123@gmail.com>
2026-04-17 14:05:40 -0500

    Update BoardManager.java

--- src/BoardManager.java
+++ src/BoardManager.java
@@ -1,7 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.macalester.graphics.Point;
+
 public class BoardManager {
 
+HashMap<Point, Card> fullBoardArray;
+
 public BoardManager(int boardSize) {
-    new Map<Card>
+    new Map<Point, Card> fullBoardArray = new HashMap<Point, Card>();
 }
 
 }
