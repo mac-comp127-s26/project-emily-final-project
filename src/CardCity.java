3a2c62a61926ef2a17823802d75c0d1656d8b475
emilyk08 <emk.emily123@gmail.com>
2026-04-27 23:22:39 -0500

    Updated "Polished" card icons and IconPathing

--- src/CardCity.java
+++ src/CardCity.java
@@ -4,7 +4,7 @@ import managers.MouseRunner;
 public class CardCity {
 
     public static void main(String[] args) {
-        MouseRunner game = new MouseRunner(650, 4, IconPath.Clean);
+        MouseRunner game = new MouseRunner(650, 4, IconPath.Polished);
         game.run();
     }
 }
--- src/enums/IconPath.java
+++ src/enums/IconPath.java
@@ -1,9 +1,10 @@
 package enums;
 
 /**
- * The two types of icons for each card in the game.
+ * The three types of icons for each card in the game.
  */
 public enum IconPath {
     Clean,
-    Basic
+    Basic,
+    Polished
 }
