7b92170639563b177b7b062269817a585ce5635e
emilyk08 <emk.emily123@gmail.com>
2026-04-30 13:46:53 -0500

    Added text scaling based on horizontal window size

--- src/ui/StatsScreen.java
+++ src/ui/StatsScreen.java
@@ -18,8 +18,8 @@ public class StatsScreen {
     private Card previewedCard;
     private Card selectedCard;
     private boolean gameOn = true;
-    private boolean textExists = false;
     private double textSpacing;
+    private double scale;
 
     private List<GraphicsText> texts = new ArrayList<>();
 
@@ -31,8 +31,17 @@ public class StatsScreen {
     /**
      * Creates a text object slightly indented from the left of the screen
      */
-    private GraphicsText createText(String text, double y) {
-        GraphicsText txt = new GraphicsText(text, canvas.getWidth() / 130, y);
+    private GraphicsText createText(String text, double y, double scale) {
+        return createText(text, canvas.getWidth()/130, y, scale);
+    }
+
+    /**
+     * Creates a text object at position (x,y)
+     */
+    private GraphicsText createText(String text, double x, double y, double scale) {
+        GraphicsText txt = new GraphicsText(text, x, y);
+        txt.setScale(scale);
+        txt.setCenter((txt.getPosition().getX()*scale)+((txt.getWidth()*scale)/2), txt.getPosition().getY());
         texts.add(txt);
         return txt;
     }
@@ -41,103 +50,97 @@ public class StatsScreen {
      * Return the GraphicsText object in texts in list that has the longest width.
      */
     private GraphicsText getLongestText() {
-        GraphicsText longest = null;
+        GraphicsText longest = texts.get(0);
         for (GraphicsText t : texts) {
             if (t.getWidth() > longest.getWidth()) {
                 longest = t;
-                System.out.println("Found something longer!");
             }
         }
         return longest;
     }
 
     private double getNewScale() {
-        double res = 1;
+        double res = 1.0;
         if (getLongestText().getWidth() > canvas.getWidth()) {
-            res = canvas.getWidth()/getLongestText().getWidth();
+            res = canvas.getWidth() / getLongestText().getWidth();
         }
         return res;
-    }
-
-    private void scaleAllText() {
-        double scale = getNewScale();
-        for (GraphicsText obj : texts) {
-            obj.setScale(scale);
-        }
     }
 
     /**
      * Reading data from @param game, update the text on the screen to match the respective values.
      */
     public void update(GameManager game) {
-        textSpacing = canvas.getHeight() / 13;
         if (gameOn) {
+            textSpacing = canvas.getHeight() / 13;
             double y = textSpacing;
             canvas.removeAll();
-            canvas.add(createText("Cards left in deck: " + game.getHand().numCardsRemaining(), y));
-            if (selectedCard != null)
-                canvas.add(new GraphicsText("Selected card:"), canvas.getWidth() - 110, y);
-            y += textSpacing;
-            if (selectedCard != null)
-                canvas.add(new GraphicsText(selectedCard.getName(), canvas.getWidth() - 110, y));
-            canvas.add(createText("Spaces left on board: " + game.getBoard().getOpenSpaces(), y));
+            canvas.add(createText("Cards left in deck: " + game.getHand().numCardsRemaining(), y, scale));
+            if (selectedCard != null) {
+                canvas.add(createText("Selected card:", canvas.getWidth() - 110, y, scale));
+                y += textSpacing;
+                canvas.add(createText(selectedCard.getName(), canvas.getWidth() - 110, y, scale));
+            } else {
+                y += textSpacing;
+            }
+            canvas.add(createText("Spaces left on board: " + game.getBoard().getOpenSpaces(), y, scale));
             y += textSpacing * 2;
-            canvas.add(createText("Population: " + game.getStats().get(0), y));
+            canvas.add(createText("Population: " + game.getStats().get(0), y, scale));
             y += textSpacing;
-            canvas.add(createText("Economy: " + game.getStats().get(1), y));
+            canvas.add(createText("Economy: " + game.getStats().get(1), y, scale));
             y += textSpacing;
-            canvas.add(createText("Leisure: " + game.getStats().get(2), y));
+            canvas.add(createText("Leisure: " + game.getStats().get(2), y, scale));
             y += textSpacing * 2;
             if (previewedCard != null) {
-                y = detailCard(previewedCard, y, textSpacing, "Previewed");
+                y = detailCard(previewedCard, y, textSpacing, scale, "Previewed");
             } else if (selectedCard != null) {
-                y = detailCard(selectedCard, y, textSpacing, "Selected");
+                y = detailCard(selectedCard, y, textSpacing, scale, "Selected");
             }
             if (game.testEndConditions() != EndCondition.NONE) {
                 y = textSpacing;
                 canvas.removeAll();
             }
             if (game.testEndConditions() == EndCondition.LOSE) {
-                canvas.add(createText("Game over!", y));
+                canvas.add(createText("Game over!", y, scale));
                 y += textSpacing * 2;
-                canvas.add(createText("Your city fell into debt.", y));
+                canvas.add(createText("Your city fell into debt.", y, scale));
                 y += textSpacing * 2;
-                canvas.add(createText("Your final scores:", y));
+                canvas.add(createText("Your final scores:", y, scale));
                 y += textSpacing;
-                canvas.add(createText("Population: " + game.getStats().get(0), y));
+                canvas.add(createText("Population: " + game.getStats().get(0), y, scale));
                 y += textSpacing;
-                canvas.add(createText("Economy: " + game.getStats().get(1), y));
+                canvas.add(createText("Economy: " + game.getStats().get(1), y, scale));
                 y += textSpacing;
-                canvas.add(createText("Leisure: " + game.getStats().get(2), y));
+                canvas.add(createText("Leisure: " + game.getStats().get(2), y, scale));
                 gameOn = false;
             } else if (game.testEndConditions() == EndCondition.WIN) {
-                canvas.add(createText("Game over!", y));
+                canvas.add(createText("Game over!", y, scale));
                 y += textSpacing * 2;
-                canvas.add(createText("Your city is thriving.", y));
+                canvas.add(createText("Your city is thriving.", y, scale));
                 y += textSpacing * 2;
                 game.runEndAbilities();
-                canvas.add(createText("Population: " + game.getStats().get(0), y));
+                canvas.add(createText("Population: " + game.getStats().get(0), y, scale));
                 y += textSpacing;
-                canvas.add(createText("Economy: " + game.getStats().get(1), y));
+                canvas.add(createText("Economy: " + game.getStats().get(1), y, scale));
                 y += textSpacing;
-                canvas.add(createText("Leisure: " + game.getStats().get(2), y));
+                canvas.add(createText("Leisure: " + game.getStats().get(2), y, scale));
                 y += textSpacing * 2;
-                canvas.add(createText("Final score: " + game.getScoreTracker().finalScore(), y));
+                canvas.add(createText("Final score: " + game.getScoreTracker().finalScore(), y, scale));
             }
+            scale = getNewScale();
         }
     }
 
     /**
      * Add the details of the selectedCard to the screen.
      */
-    public double detailCard(Card card, double y, double textHeight, String typeOfPreview) {
+    public double detailCard(Card card, double y, double textHeight, double scale, String typeOfPreview) {
         canvas.add(
-            new GraphicsText(typeOfPreview + " card: " + card.getName() + " (" + card.getTypeName() + ")"),
-            5, y);
+            createText(typeOfPreview + " card: " + card.getName() + " (" + card.getTypeName() + ")", y, scale));
         y += textHeight * 2;
-        canvas.add(new GraphicsText("Ability: ", 5, y));
+        canvas.add(createText("Ability: ", y, scale));
         y += textHeight * 2;
-        canvas.add(new GraphicsText(card.getDescription(), 5, y));
+        canvas.add(createText(card.getDescription(), y, scale));
         return y;
     }
 
